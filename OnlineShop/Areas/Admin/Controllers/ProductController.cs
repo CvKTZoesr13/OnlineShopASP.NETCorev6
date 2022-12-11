ab2423558a1cc7cf43566f68f29794bec2db8e84
CvKTZoesr13 <vanducdo4@gmail.com>
2022-12-11 16:29:11 +0700

    GET, POST Edit Action Method

--- OnlineShop/Areas/Admin/Controllers/ProductController.cs
+++ OnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -51,5 +51,54 @@ namespace OnlineShop.Areas.Admin.Controllers
             }
             return View(product);
         }
+
+        //GET Edit Action Method
+        public IActionResult Edit(int? id)
+        {
+            ViewData["productTypeId"] = new SelectList(_db.ProductTypes.ToList(), "Id", "ProductType");
+            ViewData["TagId"] = new SelectList(_db.SpecialTags.ToList(), "Id", "SpecialTag");
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var product = _db.Products.Include(c => c.ProductTypes).Include(c=>c.SpecialTag)
+                .FirstOrDefault(c=>c.Id == id);
+            if(product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // POST Edit Action Method
+        [HttpPost]
+        public async Task<IActionResult> Edit(Products product, IFormFile image)
+        {
+            if (!ModelState.IsValid) // tim hieu them ve ModelState.IsValid, tai sao data lai khong hop le! 
+            {
+                if (image != null)
+                {
+                    var name = Path.Combine(_he.WebRootPath + "/Images", Path.GetFileName(image.FileName));
+                    await image.CopyToAsync(new FileStream(name, FileMode.Create));
+                    product.Image = "Images/" + image.FileName;
+                }
+
+                if (image==null)
+                {
+                    product.Image = "Images/sixmorevodka-studio-zed-l02.jpg";
+                }
+                _db.Products.Update(product);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(product);
+        }
+
+
+        // GET Details Action Method, (chua tao view)
+        public  IActionResult Details(int? id)
+        {
+            return View();
+        }
     }
 }
